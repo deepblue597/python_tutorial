680e2f8a33c9d7cb7a55d589cf80b0eb6e230243
deepblue597 <iasonas2001@hotmail.com>
2024-05-24 21:41:12 +0300

    lesson 3 moving forward

--- lesson 3/lesson_3.py
+++ lesson 3/lesson_3.py
@@ -6,3 +6,122 @@ class Person:
     age = 22 
 
 
+Player1 = Person() 
+
+Player1.age
+# %%
+"""
+Άσκηση 
+
+Φτιάξτε μια κλάση σκύλος που να περιέχει το όνομα τη ράτσα και την ηλικία του σκύλου 
+Φτιάξτε ένα αντικείμενο σκύλο και εκτυπώστε τη ράτσα του. 
+"""
+
+class Dog: 
+    name = "fluffy"
+    race = 'golden retriever'
+    age = 3 
+
+myDog = Dog 
+
+print(myDog.race)
+
+# %%
+
+class Person: 
+
+    def __init__(self , name, age):
+        self.name = name 
+        self.age  = age 
+
+P1 = Person("Jason" , 20)
+
+print(P1.age)
+
+# %% Formula 1 
+
+"""
+Για τη διεξαγωγή των αγώνων Formula 1 χρειάζεται να δημιουργηθεί μια
+κλάση για τα αγωνιστικά αυτοκίνητα που θα τρέξουν. Φτιαξτε μια κλάση 
+η οποία θα περιέχει το όνομα της ομάδας, τα ονόματα των drivers (2) , και 
+των αριθμό τίτλων που έχει. Φτιάξτε και το αντικείμενο για την ομάδα της Ferrari 
+και εκτυπώστε των αριθμό των τίτλων που έχει 
+
+"""
+
+class Formula1: 
+    def __init__(self , name , drivers , titles):
+        self.name = name 
+        self.drivers = drivers 
+        self.titles = titles
+        
+
+Ferrari = Formula1("Ferrari" , ['LECLERC' , 'SAINZ'] , 20)
+
+print(Ferrari.titles)
+# %%
+
+class Person: 
+
+    def __init__(self , name, age):
+        self.name = name 
+        self.age  = age 
+
+    def __str__(self) -> str: # the -> str is a hint of what is returned from the function 
+        return f"Name is {self.name} and age of {self.age}"
+
+P1 = Person("Jason" , 20)
+
+print(P1)
+# %% Object functions 
+
+
+class Person: 
+
+    def __init__(self , name, age):
+        self.name = name 
+        self.age  = age 
+
+    def __str__(self) -> str:
+        return f"Name is {self.name} and age of {self.age}"
+    
+    def greeting(self) :
+        return f"Hello my name is {self.name} and i am {self.age} years old"
+
+
+P1 = Person("Michael" , 32)
+
+P1.greeting() 
+
+# %%
+
+"""
+Δημιουργήστε την κλάση Person Που έχουμε παραπάνω αλλά
+αντί του self να ονομάσεται όπως θέλετε την μεταβλητή αυτή 
+και να έχει το όνομα το επίθετο την ηλικία το τόπο γέννησης και μια 
+συνάρτηση η οποία θα επιστρέφει όλα αυτα τα χαρακτηριστικά σε μια πρόταση 
+και άλλη μια που θα τα επιστρέφει σε μια λίστα 
+"""
+
+class Person: 
+
+    def __init__(me , name , surname, age , country, ):
+        me.name = name 
+        me.surname = surname 
+        me.country  = country 
+        me.age  = age 
+
+    def __str__(me) -> str:
+        return f"Name is {me.name} and age of {me.age}"
+    
+    def greeting(me) :
+        return f"Hello my name is {me.name} and i am {me.age} years old"
+    
+    def infolist(me): 
+        return [me.name , me.surname , me.age , me.country]
+
+
+P1  = Person( "Konna" , "gka" , 23 , "Thessaloniki")
+
+P1.infolist()
+# %%
